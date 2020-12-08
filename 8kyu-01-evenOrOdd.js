65cc1debb06591f18942677851e232081e1e105d
jonghopj <jonghopj@gmail.com>
2020-12-08 13:02:22 +0900

    Fisrt commit

---
+++ 8kyu-01-evenOrOdd.js
@@ -0,0 +1,19 @@
+// Solution 01
+function evenOrOdd(n) {
+  if (n % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
+  }
+}
+
+// Solution 02
+function evenOrOdd(n) {
+  return n % 2 ? 'Odd' : 'Even';
+}
+
+console.log(evenOrOdd(0))  // Even
+console.log(evenOrOdd(1))  // Odd
+console.log(evenOrOdd(2))  // Even
+console.log(evenOrOdd(3))  // Odd
+console.log(evenOrOdd(4))  // Even
