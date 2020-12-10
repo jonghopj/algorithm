e17cfd8216f2603c4db53b6b94f10808d9d46f30
jonghopj <jonghopj@gmail.com>
2020-12-10 19:42:30 +0900

    8kyu-05-sumMix

--- 8kyu-05-sumMix1.js
+++ 8kyu-05-sumMix1.js
@@ -1,14 +1,14 @@
-// function sumMix(arr) {
-//   let result = 0;
+function sumMix(arr) {
+  let result = 0;
 
    
-//   for (let i = 0; i < arr.length; i++) {
-//     result += parseInt(arr[i]);
-//   }
-//   return result;
-// }
+  for (let i = 0; i < arr.length; i++) {
+    result += parseInt(arr[i]);
+  }
+  return result;
+}
 
-//  console.log(sumMix([1, '2', 3, '4']));
+ console.log(sumMix([1, '2', 3, '4']));
  
 // solution 2
 
