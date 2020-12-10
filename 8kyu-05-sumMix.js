e523d19c97d93c28f50564967680fff64f27353a
jonghopj <jonghopj@gmail.com>
2020-12-10 19:32:13 +0900

    8kyu-05-sumMix.js

--- 8kyu-05-sumMix.js
+++ 8kyu-05-sumMix.js
@@ -1,32 +1,33 @@
-function sumMix(arr) {
-  let result = 0;
+// function sumMix(arr) {
+//   let result = 0;
 
    
-  for (let i = 0; i < arr.length; i++) {
-    result += parseInt(arr[i]);
-  }
-  return result;
-}
+//   for (let i = 0; i < arr.length; i++) {
+//     result += parseInt(arr[i]);
+//   }
+//   return result;
+// }
 
- console.log(sumMix([1, '2', 3, '4']));
+//  console.log(sumMix([1, '2', 3, '4']));
  
-solution 2
+// solution 2
 
- function sumMix(arr) {
-   let result = 0;
+//  function sumMix(arr) {
+//    let result = 0;
 
-   for (let i = 0; i < arr.length; i++) {
-     result += Number(arr[i]);
-   }
-   return result;
- }
+//    for (let i = 0; i < arr.length; i++) {
+//      result += Number(arr[i]);
+//    }
+//    return result;
+//  }
 
- console.log(sumMix([1, '2', 3, '4']));
+//  console.log(sumMix([1, '2', 3, '4']));
 
-solution 03 
+// solution 03 
 
- function sumMix(arr) {
-    return arr.map(i => +i).reduce((result, i) => result + i, 0);
- }
+//  function sumMix(arr) {
+//     return arr.map(i => +i).reduce((result, i) => result + i, 0);
+//  }
 
- console.log(sumMix([1, '2', 3, '4']));
+//  console.log(sumMix([1, '2', 3, '4']));
+// //  askjdklasd
