7f0a9e91c0970e96f1a803ffdf7fb4aae5532024
jonghopj <jonghopj@gmail.com>
2020-12-11 21:58:03 +0900

    8kyu-06-sumWhithoutMaxAndMin

---
+++ 8kyu-06-sumWithoutMaxAndMin.js
@@ -0,0 +1,80 @@
+// function sumWhithoutMaxAndMin(arr) {
+//   if (!arr || arr.length < 3) return 0;
+
+//   let result = 0;
+//   let max = arr[0];
+//   let min = arr[0];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//     result += arr[i];
+//   }
+//   return result - max - min;
+// }
+// console.log(sumWithoutMaxAndMin([1]));
+
+// 실패
+// function sumWithoutMaxAndMin(arr) {
+//   if (!arr || arr.length < 3) return 0;
+  
+//   let result = 0;
+//   let max = arr[0];
+//   let min = arr[0];
+  
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+    
+//     if (arr[i] < min) {
+//       min = arr[i];
+//     }
+    
+//     result += arr[i];
+//   }
+  
+//   return result - max - min;
+// }
+
+// console.log(sumWithoutMaxAndMin([1]));카피
+
+
+// function sumWithoutMaxAndMin(arr) {
+//   if (!arr || arr.length < 3) return 0;
+  
+//   let result = 0;
+//   let max = min = arr[0];
+  
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] > max ? (max = arr[i]) : arr[i] < min ? (min = arr[i]) : arr[i];
+//     result += arr[i];
+//   }
+  
+//   return result - max - min;
+// }
+// console.log(sumWithoutMaxAndMin([1]));
+
+// function sumWithoutMaxAndMin(arr) {
+//   if (!arr || arr.length < 3) return 0;
+  
+//   let sorted = arr.sort((a, b) => a - b);
+//   let result = 0;
+  
+//   for (let i = 1; i < sorted.length - 1; i++) {
+//     result += sorted[i];
+//   }
+  
+//   return result;
+// }
+
+// console.log(sumWithoutMaxAndMin([1]));
+function sumWithoutMaxAndMin(arr) {
+  return arr ? arr.sort((a, b) => a - b).slice(1, -1).reduce((result, i) => result + i, 0) : 0;
+}
+
+console.log(sumWithoutMaxAndMin([1]));
