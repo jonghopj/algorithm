5ca16775c751c31283315a676abc5a4f97d41373
jonghopj <jonghopj@gmail.com>
2020-12-24 19:35:43 +0900

    8kyu-20-toBinary.js

---
+++ 8kyu-19-toBinary.js
@@ -0,0 +1,46 @@
+// function toBinary(n) {
+//   return parseInt(n.toString(2));
+// }
+
+
+
+// console.log(toBinary(1));
+
+
+// solution2
+
+// function toBinary(n) {
+//   return Number(n.toString(2));
+// }
+
+// console.log(toBinary(0));
+// console.log(toBinary(1));
+// console.log(toBinary(2)); 
+// console.log(toBinary(3)); 
+// console.log(toBinary(4));  
+// console.log(toBinary(5));  
+// console.log(toBinary(6));  
+// console.log(toBinary(7));  
+// console.log(toBinary(8));   
+// console.log(toBinary(9));   
+// console.log(toBinary(10));
+
+
+// solution3
+
+
+ function toBinary(n) {
+   return +n.toString(2);
+ }
+
+console.log(toBinary(0));
+console.log(toBinary(1));
+console.log(toBinary(2)); 
+console.log(toBinary(3)); 
+console.log(toBinary(4));  
+console.log(toBinary(5));  
+console.log(toBinary(6));  
+console.log(toBinary(7));  
+console.log(toBinary(8));   
+console.log(toBinary(9));   
+console.log(toBinary(10));
