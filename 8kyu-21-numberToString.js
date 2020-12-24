5ffb90f07691308ef547be2b1eef63ec1bb21bca
jonghopj <jonghopj@gmail.com>
2020-12-24 19:44:11 +0900

    8kyu-22-numberToString.js

---
+++ 8kyu-21-numberToString.js
@@ -0,0 +1,27 @@
+// // solution1
+
+
+//  function numberToString(num) {
+//    return '' + num;
+//  }
+
+
+//  console.log(typeof numberToString(1234));
+
+
+// solution2
+
+//  function numberToString(num) {
+
+//    return num.toString();
+//  }
+
+//  console.log(typeof numberToString(1234));
+
+// solution3
+
+ function numberToString(num) {
+   return String(num);
+ }
+
+ console.log(numberToString(1234));
