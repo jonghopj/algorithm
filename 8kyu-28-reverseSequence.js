950b57afb1eb9d8e14bae1a09091933b07259e90
jonghopj <jonghopj@gmail.com>
2020-12-26 21:21:22 +0900

    8kyu-28-reverseSequence.js

---
+++ 8kyu-28-reverseSequence.js
@@ -0,0 +1,44 @@
+// function reverseSequence(n) {
+//   let result = [];
+
+//    for (let i = n; i > 0; i --){
+//      result.push(i);
+//    }
+//    return result;
+// }
+
+
+// console.log(reverseSequence(5));
+
+
+
+// function reverseSequence(n) {
+//   return new Array(n).fill(0).map((_, index) => n - index);
+// }
+
+// console.log(reverseSequence(5));
+
+
+// function reverseSequence(n) {
+//    return new Array(n).fill(0).map((_, index) => n - index);
+
+
+// }
+
+// console.log(reverseSequence(5));
+
+// function reverseSequence(n) {
+//    return Array.from({ length: n }, (_, index) => n - index);
+// }
+
+
+// console.log(reverseSequence(5));
+
+function reverseSequence(n) {
+
+   if (n === 1) return [n];
+
+    return [n].concat(reverseSequence(n - 1));
+}
+
+console.log(reverseSequence(5));
