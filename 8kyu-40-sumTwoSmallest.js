96c18e6566b4e078965b9d6df0f1768171ecc601
jonghopj <jonghopj@gmail.com>
2020-12-29 20:00:14 +0900

    8kyu-40-sumTwoSmallest.js

---
+++ 8kyu-40-sumTwoSmallest.js
@@ -0,0 +1,38 @@
+
+function sumTwoSmallest(arr) {
+  let min0 = arr[0];
+  let min1 = arr[1];
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min0) {
+      min1 = min0;
+      min0 = arr[i];
+    } else if (arr[i] < min1) {
+      min1 = arr[i];
+    }
+  }
+  
+  return min0 + min1;
+}
+
+console.log(sumTwoSmallest([1, 2, 3, 4])); 
+
+
+
+
+function sumTwoSmallest(arr) {
+  arr = arr.sort((a, b) => a - b);
+
+  return arr[0] + arr[1];
+}
+
+console.log(sumTwoSmallest(1, 2, 3, 4));  
+
+function sumTwoSmallest(str) {
+
+   const [a, b] = arr.sort((a, b) => a - b);
+
+    return a + b;
+}
+
+ console.log(sumTwoSmallest([1, 2, 3, 4]));
