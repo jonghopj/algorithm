e27a4989d2dc10b7399c22c1da86f3c0b89932d1
jonghopj <jonghopj@gmail.com>
2021-01-01 20:21:49 +0900

    8kyu-45-findNonConsecutiveNum.js

--- 8kyu-41-countVowels.js
+++ 8kyu-41-countVowels.js
@@ -1,51 +1,51 @@
-// function countVowels(str) {
-//   const vowels = ['a', 'e', 'i', 'o', 'u'];
-//   let count = 0;
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
 
-//    for (let i = 0; i < str.length; i++) {
-//      for (let j = 0; j < vowels.length; j++) {
-//        if (str[i] === vowels[j]) {
-//          count++;
-//        }
-//      }
-//    }
+   for (let i = 0; i < str.length; i++) {
+     for (let j = 0; j < vowels.length; j++) {
+       if (str[i] === vowels[j]) {
+         count++;
+       }
+     }
+   }
 
-//     return count;
-// }
+    return count;
+}
 
-// console.log(countVowels('codewars'));
+console.log(countVowels('codewars'));
 
-// function countVowels(str) {
-//   const vowels = 'aeiou';
-//   let count = 0;
+function countVowels(str) {
+  const vowels = 'aeiou';
+  let count = 0;
 
-//    for (let i = 0; i < str.length; i++) {
-//      if (vowels.indexOf(str[i]) !== -1) {
-//        count++;
-//      }
-//    }
+   for (let i = 0; i < str.length; i++) {
+     if (vowels.indexOf(str[i]) !== -1) {
+       count++;
+     }
+   }
 
-//     return count;
-// }
+    return count;
+}
 
-//  console.log(countVowels('codewars'));
-
-
+ console.log(countVowels('codewars'));
 
 
 
 
-//  function countVowels(str) {
-//   return str.split('').filter(i => 'aeiou'.includes(i)).length;
-// }
-// console.log(countVowels('codewars'));
+
+
+ function countVowels(str) {
+  return str.split('').filter(i => 'aeiou'.includes(i)).length;
+}
+console.log(countVowels('codewars'));
 
 
 
-// function countVowels(str) {
-//   return str.replace(/[^aeiou]/g, '').length;
-// }
-// console.log(countVowels('codewars'));
+function countVowels(str) {
+  return str.replace(/[^aeiou]/g, '').length;
+}
+console.log(countVowels('codewars'));
 
 function countVowels(str) {
   return (str.match(/[aeiou]/g) || []).length;
---
+++ 8kyu-45-findNonConsecutiveNum.js
@@ -0,0 +1,37 @@
+// function findNonConsecutiveNum(arr) {
+//    for (let i = 0; i < arr.length - 1; i++) {
+//      if (arr[i] + 1 !== arr[i +1]) {
+//        return arr[i + 1];
+//      }
+//    }
+
+//    return null;
+
+
+// }
+
+//  console.log(findNonConsecutiveNum([1, 2, 3, 4]));
+
+// function findNonConsecutiveNum(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//      let current = arr[i];
+//      let previous = arr[i - 1];
+
+//      if (current - previous > 1) {
+//        return current;
+//      }
+//   }
+//    return null;
+// }
+
+// console.log(findNonConsecutiveNum([1, 2, 3, 4]));
+
+
+
+ function findNonConsecutiveNum(arr) {
+    let result = arr.find((i, index) => i !== index + arr[0]);
+
+     return Number.isInteger(result) ? result : null;
+ }
+
+ console.log(findNonConsecutiveNum([1, 2, 3, 4]));
