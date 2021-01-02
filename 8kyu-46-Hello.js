f8849027277efa8721583a298ffa521b0bda3d86
jonghopj <jonghopj@gmail.com>
2021-01-02 16:20:06 +0900

    8kyu-46-Hello.js

---
+++ 8kyu-46-Hello.js
@@ -0,0 +1,13 @@
+function sayHello(name, city, state) {
+   return 'Hello, ' + name.join(' ') + '! Welcome to ' + city + ',  ' + state + '!';
+}
+
+ console.log(sayHello(['Sam', 'Azor'], 'Dubbo', 'New South Wales'));
+
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
+
+
+console.log(sayHello(['Sam', 'Azor'], 'Dubbo', 'New South Wales'));
