0fc501ce129ded81e304ce0319d4805c692ac0be
jonghopj <jonghopj@gmail.com>
2021-03-01 17:10:49 +0900

    8kyu-findNeedle.js

---
+++ 8kyu-findNeedle.js
@@ -0,0 +1,24 @@
+Haystacks
+const haystack_01 = [4, '2019', undefined, 'needle'];
+const haystack_02 = ['thread', true, 'needle', 2019];
+const haystack_03 = [false, 'thread', 8688, 'code'];
+
+// solution1
+
+// function findNeedle(haystack) {
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === 'needle') {
+//       return i;
+//     }
+//   }
+// }
+
+// console.log(findNeedle(haystack_01));
+
+// solution2
+
+function findNeedle(haystack) {
+  return haystack.indexOf('needle');
+}
+
+console.log(findNeedle(haystack_03));
